fcb5192c8fe806054438dbf101b27dd5b15e0417
Mariano Carnevale <marianocarne900@gmail.com>
2024-01-25 17:02:42 +0100

    Agregador el modal con un boton de apertura y cerrado, animaciones OK

---
+++ modal.js
@@ -0,0 +1,15 @@
+"use strict";
+const modalBtn = document.querySelector(".modal_container_btn_img");
+const modal = document.querySelector("#customCardForm");
+const closeBtn = document.querySelector(".close_form");
+
+modalBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+}
